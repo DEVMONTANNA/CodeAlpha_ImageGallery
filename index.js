0a96414327d6e3bb9cbac5c24ce88799813edee1
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-08-24 13:23:58 +0100

    new repo

---
+++ index.js
@@ -0,0 +1,38 @@
+const add = document.querySelector(".add");
+add.addEventListener("click", function () {
+  alert("hello goodluck");
+});
+
+const darkmode = document.querySelector(".darkmode");
+const lightmode = document.querySelector(".lightmode");
+
+darkmode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  darkmode.style.display = "none";
+  setTimeout(() => {
+    lightmode.style.display = "inline-block";
+  }, 100);
+});
+
+lightmode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+  lightmode.style.display = "inline-block";
+  setTimeout(() => {
+    lightmode.style.display = "none";
+    darkmode.style.display = "inline-block";
+  }, 2000);
+});
+
+
+ function openLightbox(src, caption) {
+    document.getElementById("lightbox-img").src = src;
+    document.getElementById("lightbox-caption").innerText = caption;
+    document.getElementById("lightbox").classList.remove("hidden");
+    document.getElementById("lightbox").classList.add("flex");
+  }
+  function closeLightbox() {
+    document.getElementById("lightbox").classList.remove("flex");
+    document.getElementById("lightbox").classList.add("hidden");
+  }
